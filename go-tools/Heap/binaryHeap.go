3b266886ff976f9bfaa85ad1f20a3e73f10371e1
DISHI-CN\dianyue <dainyue.shi@oracle.com>
2018-10-21 09:04:44 +0800

    add heap with go

---
+++ go-tools/Heap/binaryHeap.go
@@ -0,0 +1,30 @@
+package main
+
+type HeapType int
+
+const (
+	MAX_HEAP HeapType iota
+	MIN_HEAP 
+)
+
+type Heap struct {
+	values []int
+	heapType int
+}
+
+func (heap *Heap) generateHeap(src []int) Heap{
+
+}
+
+func (heap *Heap) add(item int) Heap {
+	values := heap.values
+}
+
+// pop the root item
+func (head *Heap) pop() int {
+
+}
+
+func adjustHeap(src []int) {
+	
+}
